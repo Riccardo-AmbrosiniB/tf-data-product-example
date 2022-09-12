794bf9c1bb9fe80b82ae11985df438638e80fb1d
Giandomenico Avelluto <giandomenico.avelluto@quantyca.it>
2022-09-12 14:52:23 +0200

    aggiunto output s3 di dev

--- modules/datalake/output.tf
+++ modules/datalake/output.tf
@@ -2,3 +2,7 @@ output "productionDataLakeEndpoint" {
   value = aws_s3_bucket.s3_production_zone.bucket_regional_domain_name
 }
 
+output "devDataLakeEndpoint" {
+  value = aws_s3_bucket.s3_development_zone.bucket_regional_domain_name
+}
+
--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "productionDataLakeEndpoint" {
   value = module.datalake.productionDataLakeEndpoint
 }
+
+output "devDataLakeEndpoint" {
+  value = module.datalake.devDataLakeEndpoint
+}
+
