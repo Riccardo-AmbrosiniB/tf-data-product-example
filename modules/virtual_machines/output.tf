8d9c3259b089b1dfd4df7ff51c91c024b463af47
Giandomenico Avelluto <giandomenico.avelluto@quantyca.it>
2022-10-18 16:30:41 +0200

    modificato output vm azure

--- modules/virtual_machines/output.tf
+++ modules/virtual_machines/output.tf
@@ -2,3 +2,6 @@ output "vmPrivateIp" {
   value = azurerm_linux_virtual_machine.odm_demo.private_ip_address
 }
 
+output "vmPublicIp" {
+  value = azurerm_linux_virtual_machine.odm_demo.private_ip_address
+}
--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "vmPrivateIp" {
   value = module.virtual_machines.vmPrivateIp
 }
 
+output "vmPublicIp" {
+  value = module.virtual_machines.vmPublicIp
+}
+
 #output "devDataLakeEndpoint" {
 #  value = module.datalake.devDataLakeEndpoint
 #}
