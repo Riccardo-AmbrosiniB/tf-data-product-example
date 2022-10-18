980c541dd29da1f02d6edb4f37fe5b49d0fefe0a
Giandomenico Avelluto <giandomenico.avelluto@quantyca.it>
2022-10-18 17:01:28 +0200

    fix nome security group

--- modules/virtual_machines/output.tf
+++ modules/virtual_machines/output.tf
@@ -3,5 +3,5 @@ output "vmPrivateIp" {
 }
 
 output "vmPublicIp" {
-  value = azurerm_linux_virtual_machine.odm_demo.private_ip_address
+  value = azurerm_linux_virtual_machine.odm_demo.public_ip_address
 }
