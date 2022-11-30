67960691fc7b9bf7c6a9c63a072091c2fe8a32ed
Riccardo-AmbrosiniB <61760259+Riccardo-AmbrosiniB@users.noreply.github.com>
2022-11-30 15:51:18 +0100

    Update main.tf

--- modules/datalake/main.tf
+++ modules/datalake/main.tf
@@ -1,11 +1,29 @@
 resource "aws_s3_bucket" "s3_raw_zone" {
   bucket = "${var.lake_name}-landing-zone"
+  timeouts {
+    create = "15s"
+    delete = "15s"
+    read = "15s"
+    update = "15s"
+  }
 }
 
 resource "aws_s3_bucket" "s3_development_zone" {
   bucket = "${var.lake_name}-development-zone"
+  timeouts {
+    create = "15s"
+    delete = "15s"
+    read = "15s"
+    update = "15s"
+  }
 }
 
 resource "aws_s3_bucket" "s3_production_zone" {
   bucket = "${var.lake_name}-production-zone"
+  timeouts {
+    create = "15s"
+    delete = "15s"
+    read = "15s"
+    update = "15s"
+  }
 }
